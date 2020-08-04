2d0c21d990d016872633ca4dc6cfaf999b223c3f
ASVECode <57017077+ASVECode@users.noreply.github.com>
2020-08-04 16:00:45 +0300

    Add files via upload

---
+++ BooksTotalPrice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01.BooksTotalPrice
+{
+    class BooksTotalPrice
+    {
+        static void PrintTotalAmountForBooks(decimal[] prices)
+        {
+            decimal totalAmount = 0;
+            foreach (decimal singleBookPrice in prices)
+            {
+                totalAmount += singleBookPrice;
+            }
+            Console.WriteLine("The total amount of all books is: " + totalAmount);
+        }
+        static void Main(string[] args)
+        {
+            decimal[] booksPrice = { 10.50m, 15.70m, 7, 49 };
+            PrintTotalAmountForBooks(booksPrice);
+        }
+    }
+}
